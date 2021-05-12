c49d4a5e1018c3715c5d1f423f31ae7525feeda3
Hifz ur Rehman Ali <aliijaz2010@yahoo.com>
2021-05-12 06:19:30 +0500

    added tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { TodoItem } from "./components/todo";
+test("renders to check title of the app", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/todo app/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("to check the button to be clicked once", () => {
+  const handleSubmit = jest.fn();
+  render(<button onClick={handleSubmit}>Button</button>);
+  fireEvent.click(screen.getByText(/button/i));
+  expect(handleSubmit).toHaveBeenCalledTimes(1);
+});
+
+test("to check the div element in the todoItem func", () => {
+  const item = {
+    title: "Ali",
+  };
+  const { getByTestId } = render(<TodoItem todo={item} />);
+  expect(getByTestId("todo-title")).toBeInTheDocument();
 });
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -2,7 +2,7 @@ import "../App.css";
 
 export const TodoItem = (params) => {
   return (
-    <div>
+    <div data-testid="todo-title">
       <p>{params.todo.title}</p>
     </div>
   );
